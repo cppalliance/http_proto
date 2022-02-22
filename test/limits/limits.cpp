dc92ac4e05ad8b1b4b60ec8924b2f0cfa1167598
Vinnie Falco <vinnie.falco@gmail.com>
2022-02-21 19:33:52 -0800

    Add limits tests

---
+++ test/limits/limits.cpp
@@ -0,0 +1,31 @@
+//
+// Copyright (c) 2019 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/CPPAlliance/http_proto
+//
+
+#include <boost/http_proto.hpp>
+
+#include "test_suite.hpp"
+
+namespace boost {
+namespace urls {
+
+class limits_test
+{
+public:
+    void
+    run()
+    {
+    }
+};
+
+TEST_SUITE(
+    limits_test,
+    "boost.http_proto.limits");
+
+} // http_proto
+} // boost
