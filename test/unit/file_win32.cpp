e7cbf0cb1145a659a26d9c713a6d22663ec7b119
Christian Mazakas <christian.mazakas@gmail.com>
2023-12-20 15:19:53 -0800

    Disable certain file I/O tests for mingw libstdc++ versions
    See https://github.com/boostorg/filesystem/issues/298 for more information.

--- test/unit/file_win32.cpp
+++ test/unit/file_win32.cpp
@@ -10,6 +10,20 @@
 // Test that header file is self-contained.
 #include <boost/http_proto/file_win32.hpp>
 
+#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32) ||                 \
+    defined(__CYGWIN__)
+#define BOOST_HTTP_PROTO_IS_WIN
+#endif
+
+#if defined(BOOST_NO_RTTI) && defined(BOOST_HTTP_PROTO_IS_WIN) &&              \
+    defined(BOOST_GCC)
+
+#include <boost/config/pragma_message.hpp>
+
+BOOST_PRAGMA_MESSAGE("skipping file_win32_ tests for this configuration")
+
+#else
+
 #if BOOST_HTTP_PROTO_USE_WIN32_FILE
 
 #include "file_test.hpp"
@@ -35,4 +49,5 @@ TEST_SUITE(
 } // http_proto
 } // boost
 
+#endif // BOOST_HTTP_PROTO_USE_WIN32_FILE
 #endif
