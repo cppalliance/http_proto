842da860210ea56574958afb11a3d200eee0c1d6
Christian Mazakas <christian.mazakas@gmail.com>
2024-06-04 10:24:55 -0700

    detail::downcast throws on target mismach

--- include/boost/http_proto/detail/type_index.hpp
+++ include/boost/http_proto/detail/type_index.hpp
@@ -11,55 +11,79 @@
 #ifndef BOOST_HTTP_PROTO_DETAIL_TYPE_INDEX_HPP
 #define BOOST_HTTP_PROTO_DETAIL_TYPE_INDEX_HPP
 
+#include <boost/config.hpp>
+#include <boost/assert/source_location.hpp>
 #include <boost/container_hash/hash.hpp>
 #include <boost/core/typeinfo.hpp>
-#include <boost/config.hpp>
+#include <boost/throw_exception.hpp>
 
 namespace boost {
 namespace http_proto {
 namespace detail {
 
-struct type_index_impl {
+struct type_index_impl
+{
 private:
   boost::core::typeinfo const *pdata_ = nullptr;
 
-  std::size_t get_raw_name_length() const noexcept {
+  std::size_t
+  get_raw_name_length() const noexcept
+  {
     // Boost.TypeIndex has a dramatically more sophisticated implementation here
     // see if this eventually turns out to matter and if it does, essentially
     // just do more copy-paste
     return std::strlen(raw_name());
   }
 
-  bool equal(type_index_impl const &rhs) const noexcept {
+  bool
+  equal(type_index_impl const &rhs) const noexcept
+  {
     return raw_name() == rhs.raw_name() ||
            !std::strcmp(raw_name(), rhs.raw_name());
   }
 
 public:
-  type_index_impl(boost::core::typeinfo const &type_info) noexcept
+  type_index_impl(
+      boost::core::typeinfo const &type_info) noexcept
       : pdata_(&type_info) {}
 
   type_index_impl(type_index_impl const &) = default;
-  type_index_impl &operator=(type_index_impl const &) = default;
+  type_index_impl& operator=(
+      type_index_impl const &) = default;
 
   ~type_index_impl() = default;
 
-  template <class T> static type_index_impl type_id() noexcept {
-    return type_index_impl(BOOST_CORE_TYPEID(T));
+  template <class T>
+  static
+  type_index_impl
+  type_id() noexcept
+  {
+      return type_index_impl(BOOST_CORE_TYPEID(T));
   }
 
-  char const *raw_name() const noexcept { return pdata_->name(); }
-
-  std::size_t hash_code() const noexcept {
-    return boost::hash_range(raw_name(), raw_name() + get_raw_name_length());
+  char const*
+  raw_name() const noexcept
+  {
+      return pdata_->name();
   }
 
-  bool operator==(type_index_impl const &rhs) const noexcept {
-    return equal(rhs);
+  std::size_t
+  hash_code() const noexcept
+  {
+      return boost::hash_range(
+        raw_name(), raw_name() + get_raw_name_length());
   }
 
-  bool operator!=(type_index_impl const &rhs) const noexcept {
-    return !equal(rhs);
+  bool
+  operator==(type_index_impl const &rhs) const noexcept
+  {
+      return equal(rhs);
+  }
+
+  bool
+  operator!=(type_index_impl const &rhs) const noexcept
+  {
+      return !equal(rhs);
   }
 };
 
@@ -67,25 +91,48 @@ public:
 // but without requiring RTTI
 using type_index = type_index_impl;
 
-template <class T> type_index get_type_index() noexcept {
+template <class T>
+type_index
+get_type_index() noexcept
+{
   return type_index_impl::type_id<T>();
 }
 
-struct type_index_hasher {
-  std::size_t operator()(type_index const &tid) const noexcept {
+struct type_index_hasher
+{
+  std::size_t
+  operator()(type_index const &tid) const noexcept
+  {
     return tid.hash_code();
   }
 };
 
-template <class U, class T> U downcast(T *p) {
+BOOST_NOINLINE BOOST_NORETURN
+inline
+void
+throw_bad_cast(boost::source_location const& loc = {})
+{
+  boost::throw_exception(std::bad_cast(), loc);
+}
+
+template <class U, class T>
+U
+downcast(T *p)
+{
 #ifdef BOOST_NO_RTTI
   return static_cast<U>(p);
 #else
-  return dynamic_cast<U>(p);
+  auto q = dynamic_cast<U>(p);
+  if(! q )
+    throw_bad_cast();
+  return q;
 #endif
 }
 
-template <class U, class T> U downcast(T &p) {
+template <class U, class T>
+U
+downcast(T &p)
+{
 #ifdef BOOST_NO_RTTI
   return static_cast<U>(p);
 #else
