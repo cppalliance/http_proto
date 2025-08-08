3cedee9f23c4409a3d1a2284e1f03e2816003066
Mohammad Nejati <ashtumashtum@gmail.com>
2025-08-08 16:26:13 +0000

    reintroduce string_body

---
+++ include/boost/http_proto/string_body.hpp
@@ -0,0 +1,103 @@
+//
+// Copyright (c) 2021 Vinnie Falco (vinnie.falco@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/cppalliance/http_proto
+//
+
+#ifndef BOOST_HTTP_PROTO_STRING_BODY_HPP
+#define BOOST_HTTP_PROTO_STRING_BODY_HPP
+
+#include <boost/http_proto/detail/config.hpp>
+#include <boost/buffers/const_buffer.hpp>
+#include <string>
+#include <utility>
+
+namespace boost {
+namespace http_proto {
+
+/** A ConstBufferSequence adapter for an owned `std::string`.
+
+    Takes ownership of a `std::string` and exposes
+    it via an interface conforming to the
+    ConstBufferSequence requirements.
+
+    @par Example
+    @code
+    serializer sr(ctx);
+    response res(status::not_found);
+
+    std::string body =
+        "<html>\n"
+        "    <body>\n"
+        "        <h1>404 Not Found</h1>\n"
+        "        <p>Sorry, the page does not exist.</p>\n"
+        "    </body>\n"
+        "</html>\n";
+    sr.start<string_body>(res, std::move(body));
+    @endcode
+
+    @see
+        @ref serializer.
+*/
+class string_body
+{
+    std::string s_;
+    buffers::const_buffer cb_;
+
+public:
+    /// The type for each buffer.
+    using value_type = buffers::const_buffer;
+
+    /// The type of a const iterator.
+    using const_iterator = buffers::const_buffer const*;
+
+    string_body(
+        string_body&& other) noexcept
+        : s_(std::move(other.s_))
+        , cb_(s_.data(), s_.size())
+    {
+        other.cb_ = {};
+    }
+
+    /** Constructor.
+    */
+    string_body(
+        string_body const& other) = delete;
+
+    /** Constructor.
+
+        @param s The string to take ownership of.
+    */
+    string_body(
+        std::string s) noexcept
+        : s_(std::move(s))
+        , cb_(s_.data(), s_.size())
+    {
+    }
+
+    /** Return an iterator to the beginning of the
+        buffer sequence.
+    */
+    const_iterator
+    begin() const noexcept
+    {
+        return &cb_;
+    }
+
+    /** Return an iterator to the end of the
+        buffer sequence.
+    */
+    const_iterator
+    end() const noexcept
+    {
+        return &cb_ + 1;
+    }
+};
+
+} // http_proto
+} // boost
+
+#endif
